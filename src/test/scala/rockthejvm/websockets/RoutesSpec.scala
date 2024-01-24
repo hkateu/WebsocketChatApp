d59e7bc5b6c984454fe48becf7247dae8a752ee3
kateu herbert <hkateu@gmail.com>
2024-01-25 02:10:00 +0300

    Test need update

--- src/test/scala/rockthejvm/websockets/RoutesSpec.scala
+++ src/test/scala/rockthejvm/websockets/RoutesSpec.scala
@@ -8,31 +8,14 @@ import org.http4s.Method
 import org.http4s.implicits.uri
 import org.http4s.Status
 import org.http4s.server.websocket.WebSocketBuilder2
-// import org.http4s.Response
-// import org.http4s.client.websocket.{WSClient, WSConnection}
 
 class HelloWorldSpec extends CatsEffectSuite:
-  test("/chat.html returns status code 200") {
-    val chatPageStatus = WebSocketBuilder2[IO].flatMap{wsb => 
-      service[IO](wsb)
-        .run(Request(method = Method.GET, uri = uri"/chat.html"))
-        .map(_.status)  
-    }
-    assertIO(chatPageStatus, Status.Ok)
-  }
-
-  // test("/ws test"){
-  //   val connect = WSConnection
-  //  WSClient[IO](false)()
-  //   // println(retHelloWorld.flatMap(_.as[String]))
-  //   assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
+  ???
+  // test("/chat.html returns status code 200") {
+  //   val chatPageStatus = WebSocketBuilder2[IO].flatMap{wsb => 
+  //     service[IO](wsb)
+  //       .run(Request(method = Method.GET, uri = uri"/chat.html"))
+  //       .map(_.status)  
+  //   }
+  //   assertIO(chatPageStatus, Status.Ok)
   // }
-
-  // test("HelloWorld returns hello world message") {
-  //   assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
-  // }
-
-  // private[this] val retHelloWorld: IO[Response[IO]] =
-  //   val getHW = Request[IO](Method.GET, uri"/ws")
-  //   val chatPageStatus = WebSocketBuilder2[IO].flatMap(service[IO](_).run(getHW))
-  //   chatPageStatus
